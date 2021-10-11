b616ab92aa7eed4a278aecde9cb1aa9eac1d892a
Morten Jessen <mortenjessen5d@gmail.com>
2021-10-11 21:35:47 +0200

    Implemented News service and card

---
+++ Werk.Services/News/NewsOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Werk.Services.News
+{
+    public class NewsOptions
+    {
+        public IEnumerable<NewsSource> Sources { get; set; }
+
+        public class NewsSource
+        {
+            public string Name { get; set; }
+
+            public string Url { get; set; }
+
+            public string Regex { get; set; }
+        }
+    }
+}
